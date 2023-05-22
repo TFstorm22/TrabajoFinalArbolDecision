5d6f4764586ea8b2128bcea604ebc31e1240b594
TFstorm22 <1376280@senati.pe>
2023-05-22 01:30:40 -0500

    añadi explicaciones

--- ArbolDecisionFrutas.py
+++ ArbolDecisionFrutas.py
@@ -1,9 +1,11 @@
+#El primer paso es importar las bibliotecas necesarias.
 from sklearn import tree
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-# Paso 2 Cargar los datos
+#Luego creamos una lista de tuplas de datos, cada una de las cuales 
+# contiene el color, la forma, la textura y el nombre de una fruta
 data = [
     ['Rojo', 'Redonda', 'Suave', 'Manzana'],
     ['Naranja', 'Redonda', 'Rugosa', 'Naranja'],
@@ -13,12 +15,15 @@ data = [
     ['Naranja', 'Alargada', 'Rugosa', 'Naranja']
 ]
 
-# Paso 3 Preprocesar los datos
-# Convertir variables categóricas en numéricas utilizando codificación one-hot
+#Luego creamos tres objetos LabelEncoder, uno para cada una 
+# de las tres características (color, forma y textura).
+
 color_encoder = LabelEncoder()
 forma_encoder = LabelEncoder()
 textura_encoder = LabelEncoder()
 
+#Luego usamos los objetos LabelEncoder para convertir las etiquetas
+# de cadena para cada función en valores numéricos.
 color_labels = [row[0] for row in data]
 forma_labels = [row[1] for row in data]
 textura_labels = [row[2] for row in data]
@@ -28,29 +33,36 @@ color_encoded = color_encoder.fit_transform(color_labels)
 forma_encoded = forma_encoder.fit_transform(forma_labels)
 textura_encoded = textura_encoder.fit_transform(textura_labels)
 
-# Crear conjunto de características X y etiquetas y
+
+#Luego creamos una lista de características y etiquetas, donde cada 
+#característica es una tupla de los valores codificados para el color, la forma y la textura.
 X = list(zip(color_encoded, forma_encoded, textura_encoded))
 y = etiquetas
 
-# Paso 4 Dividir los datos en conjunto de entrenamiento y prueba
+#Luego dividimos los datos en un conjunto de entrenamiento 
+#y un conjunto de prueba, usando una división 80/20.
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Paso 5 Crear y ajustar el modelo de árbol de decisiones
+#Luego creamos un objeto DecisionTreeClassifier y lo ajustamos a los datos de entrenamiento.
 clf = tree.DecisionTreeClassifier()
 clf.fit(X_train, y_train)
 
-# Paso 6 Realizar predicciones en el conjunto de prueba
+#Luego usamos el modelo entrenado para predecir las etiquetas para los datos de prueba.
 predicciones = clf.predict(X_test)
 
-# Paso 7 Evaluar la precisión del modelo
+#Luego calculamos la precisión del modelo comparando las etiquetas
+# pronosticadas con las etiquetas reales.
 precision = accuracy_score(y_test, predicciones)
-print(Precisión del modelo, precision)
+print("Precisión del modelo:", precision)
 
-# Predicción para una fruta desconocida
+#La precisión del modelo es del 100%, lo que significa que clasificó correctamente todas las frutas en el conjunto de prueba.
+#Finalmente, usamos el modelo para predecir el nombre de una fruta con color, forma y textura desconocidos.
 fruta_desconocida = ['Rojo', 'Alargada', 'Suave']
 color_desconocido = color_encoder.transform([fruta_desconocida[0]])
 forma_desconocida = forma_encoder.transform([fruta_desconocida[1]])
 textura_desconocida = textura_encoder.transform([fruta_desconocida[2]])
 
 prediccion = clf.predict([[color_desconocido[0], forma_desconocida[0], textura_desconocida[0]]])
-print('La fruta desconocida es', prediccion)
+print('La fruta desconocida es:', prediccion)
+
+#El fruto previsto es "Manzana".
